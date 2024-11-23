bb7fa70001c5b17980abec1bc657c62590a24953
roshanhoro <roshanmunda028@gmail.com>
2024-11-23 10:42:51 +0530

    t commit

---
+++ js/music-list.js
@@ -0,0 +1,200 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Justin Bieber - What Do You Mean",
+    artist: "Justin Bieber",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Badlapur - Jeena Jeena ",
+    artist: "Atif Aslam",
+    img: "music-8",
+    src: "music-8",
+  },
+  {
+    name: "Kimi_no_Na_wa_(Your_Name)",
+    artist: "Radwimps",
+    img: "music-9",
+    src: "music-9",
+  },
+  {
+    name: "Clean_Bandit_-_Rockabye_",
+    artist: "Sean_Paul_&_Anne-Marie",
+    img: "music-10",
+    src: "music-10",
+  },
+  {
+    name: "Shape_Of_You",
+    artist: "Ed_Sheeran",
+    img: "music-11",
+    src: "music-11",
+  },
+  {
+    name: "Serena_Safari",
+    artist: "AR_Creation",
+    img: "music-12",
+    src: "music-12",
+  },
+  {
+    name: "Walk off the Earth",
+    artist: "My Stupid Heart",
+    img: "music-13",
+    src: "music-13",
+  },
+  {
+    name: "Left and Right",
+    artist: "Charlie Puth",
+    img: "music-14",
+    src: "music-14",
+  },
+  {
+    name: "Dandelions",
+    artist: "Ruth B.",
+    img: "music-15",
+    src: "music-15",
+  },
+  {
+    name: "Lucifer was innocent",
+    artist: "3am in hell",
+    img: "music-16",
+    src: "music-16",
+  },
+  {
+    name: "Snap Baarishen",
+    artist: "Rosa linn",
+    img: "music-17",
+    src: "music-17",
+  },
+  {
+    name: "One Kiss i was never there slowed",
+    artist: "Calvin Harris",
+    img: "music-18",
+    src: "music-18",
+  },
+  {
+    name: "SNAP Rose Linn",
+    artist: "rose linn",
+    img: "music-19",
+    src: "music-19",
+  },
+  {
+    name: "Golden hour",
+    artist: "JVKE",
+    img: "music-20",
+    src: "music-20",
+  },
+  {
+    name: "The Chainsmoker- Closer",
+    artist: "Halsey",
+    img: "music-21",
+    src: "music-21",
+  },
+  {
+    name: "The Chainsmoker-Paris",
+    artist: "Halsey",
+    img: "music-22",
+    src: "music-22",
+  },
+  {
+    name: "The Chainsmoker- All we know ",
+    artist: "Phoebe Ryan",
+    img: "music-23",
+    src: "music-23",
+  },
+  {
+    name: "The Chainsmoker-Hope",
+    artist: "Winona Oak",
+    img: "music-24",
+    src: "music-24",
+  },
+  {
+    name: "On Fly On ",
+    artist: "Josiah Nichols",
+    img: "music-25",
+    src: "music-25",
+  },
+  {
+    name: "Fifty Fifty",
+    artist: "Cupid",
+    img: "music-26",
+    src: "music-26",
+  },
+  {
+    name: "Clam Down",
+    artist: "Selena Gomez",
+    img: "music-27",
+    src: "music-27",
+  },
+  {
+    name: "Shyad Love Aj Kal",
+    artist: "Pritam,Arjit",
+    img: "music-28",
+    src: "music-28",
+  },
+  {
+    name: "Let her go Love songs",
+    artist: "love songs",
+    img: "music-29",
+    src: "music-29",
+  },
+  {
+    name: "Acoustic cover songs",
+    artist: "Unstoppable",
+    img: "music-30",
+    src: "music-30",
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
